5be78eb582ddf8621ec9fa0049fac7d825f9e538
abanoubha <abanoubhannaa73@gmail.com>
2022-04-10 22:02:22 +0200

    1st working example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/abanoubha/gtk4-examples
 
 go 1.18
+
+require github.com/abanoubha/gtk v0.1.3
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/abanoubha/gtk"
+)
+
+func main() {
+	what := gtk.UseGtk()
+	fmt.Println("using gtk status :", what)
 }
